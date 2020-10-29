68bc00adf7447b0204ff61b933aa6dd8f7e1c29a
Kasun Suess <ks_ext@marktplatz-agentur.de>
2020-10-29 15:50:38 +0200

    add new components to main

--- bandd/src/app/app.module.ts
+++ bandd/src/app/app.module.ts
@@ -3,17 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostCarComponent } from './shared/components/post-car/post-car.component';
-import { PostSpareComponent } from './shared/components/post-spare/post-spare.component';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainLayoutComponent,
-    PostCarComponent,
-    PostSpareComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
